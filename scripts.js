21e1bf979d7729d8811d8a9ebd44c9ef33b44ee1
SongDogDevLead <176419206+SongDogDevLead@users.noreply.github.com>
2024-08-13 13:26:13 -0500

    Add playRound function and score management

--- scripts.js
+++ scripts.js
@@ -1,12 +1,22 @@
+
+// make score variables
+let playerScore = 0;
+let computerScore = 0;
+
+//create a function for scoring rounds
+function playRound(playerMove, computerMove){
+
 // Create a variable to store a generated number
 let numberGen = 0;
 // Create a Variable to store the computer choice
 let computerMove;
+
 // Find the computer choice
 function getComputerChoice () {
+
 // Generate a random number between 0 and 1
 numberGen = Math.random();
-console.log(numberGen);
+
 // Set each variable to correspond to a range of results split evenly between the options
 if (numberGen > .33 && numberGen < .67) {
     computerMove = `Rock`
@@ -18,23 +28,65 @@ else {computerMove = `Scissors`};
 // Return the proper string for the generated value
 return computerMove;
 };
-console.log(getComputerChoice());
 
 //Create variable to store player input and player move
 let playerInput;
 let playerMove;
+
 // Ask for and log Player choice
 // Create a Prompt for player input
 
 playerInput = window.prompt(`What's your move?`, `Rock, Paper, or Scissors?`);
 let normalizedInput = playerInput.toLowerCase();
+
 // Parse the input into a valid move
 function getHumanChoice(){
 if (normalizedInput === `rock`) {playerMove = `Rock`}
 else if (normalizedInput === `paper`) {playerMove = `Paper`}
 else if (normalizedInput === `scissors`) {playerMove = `Scissors`}
 else {playerMove =`Invalid Move`}
+
 // return player move
 return playerMove;};
 
-console.log(getHumanChoice())
+// Assign scoring parameters, log output and tally score
+    if(playerMove === `Rock` && computerMove === `Rock`) {
+        console.log(`Its a tie! Rock vs Rock.`)}
+    else if(playerMove === `Rock` && computerMove === `Paper`){
+        console.log(`Computer wins! Paper beats Rock!`);
+         if (computerScore < 5) {computerScore++}}
+    else if(playerMove === `Rock` && computerMove === `Scissors`){
+        console.log(`You win! Rock beats Scissors!`);
+        if (playerScore < 5) {playerScore++}}
+   else if (playerMove === `Paper` && computerMove === `Rock`) {
+    console.log(`You win! Paper beats Rock!`);
+    if (playerScore < 5) {playerScore++}}
+   else if(playerMove === `Paper` && computerMove === `Paper`){
+    console.log(`Its a tie! Paper vs Paper.`)}
+   else if(playerMove === `Paper` && computerMove === `Scissors`){
+    console.log(`Computer wins! Scissors beats Paper!`); 
+    if (computerScore < 5) {computerScore++}}
+   else if(playerMove === `Scissors` && computerMove === `Rock`) {
+    console.log(`Computer wins! Rock beats Scissors!`); 
+    if (computerScore < 5) {computerScore++}}
+    else if(playerMove === `Scissors` && computerMove === `Paper`){
+        console.log(`You win! Scissors beats Paper!`); 
+        if (playerScore < 5) {playerScore++}}
+    else if(playerMove === `Scissors` && computerMove === `Scissors`){
+        console.log(`Its a tie! Scissors vs Scissors!`)}
+    else{console.log(`Invalid Input`)}
+}
+
+function playGame(){
+playRound(playerMove, computerMove);
+if (computerScore === 5){
+    console.log(`Game Over! You lose.`);
+    computerScore = 0;
+    playerScore = 0;}
+else if (playerScore === 5){
+    console.log(`Game Over! You Win. Your score verus computer: ` + playerScore + `/` + computerScore);
+    computerScore = 0;
+    playerScore = 0;}
+else{console.log(`Next Round`);
+}
+}
